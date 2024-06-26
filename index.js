e21e5e76d7fc6fdd7c028cb39b7724e37e888f87
Nithin-2005-dev <bingu_ug_23@ee.nits.ac.in>
2024-06-26 11:51:26 +0530

    hi

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const addInputs=()=>{
-    if(inputTodo==""){
+    console.log("called")
+    if(!inputTodo || !inputDate || !inputTime){
         return;
-    }
-    items.push({todo:inputTodo,date:inputDate,time:inputTime});
+    }else{
+    items.push({todo:inputTodo,date:inputDate,time:inputTime,shake:[inputTime]});
     todoEvent.target.value="";
     dateEvent.target.value="";
     timeEvent.target.value="";
     localStorage.setItem("todos",JSON.stringify(items));
     addItems();
+    }
 }
 const deleteItem=(e)=>{
     items.splice(e.target.id,1);
@@ -26,6 +28,7 @@ const addItems=()=>{
         <button id=${i} class="delete items but" onclick=deleteItem(event)>delete</button>
     </div>`
     }
+    check();
 }
 document.getElementById("add").addEventListener("click",()=>{addInputs()});
 let items=JSON.parse(localStorage.getItem("todos")) ?? [];
@@ -35,6 +38,8 @@ let inputTime="";
 let todoEvent="";
 let dateEvent="";
 let timeEvent="";
+let time=new Date();
+let getTime=time.getTime();
 document.getElementById("todo-input").addEventListener("change",(event)=>{
     inputTodo=event.target.value;
     todoEvent=event;
@@ -47,4 +52,32 @@ document.getElementById("date-input").addEventListener("change",(event)=>{
     inputDate=event.target.value;
     dateEvent=event;
 })
+const innerCheck=(element)=>{
+    let month="";
+    if(time.getMonth().length==2) month=time.getMonth()+1;
+    else month="0"+(time.getMonth()+1);
+console.log(element.date)
+console.log(time.getFullYear()+"-"+(month)+"-"+time.getDate())
+if(element.shake[0]<=time.toTimeString().substring(0,5) && element.date<=time.getFullYear()+"-"+month+"-"+time.getDate()){
+    console.log("entered")
+    return true;
+}
+return false;
+}
+const check=()=>{
+    let newItems=JSON.parse(localStorage.getItem("todos")) || [];
+    let index=0;
+    newItems.forEach(element => {
+        if(innerCheck(element)===true){
+            document.getElementById(`${index}`).classList.add("shake-it");
+            console.log(document.getElementById(`${index}`).classList)
+            console.log(index+"p")
+        }else{
+            document.getElementById(`${index}`).classList.remove("shake-it"); 
+            console.log(index)
+        }
+        index++;
+    });
+}
 addItems();
+check()
